466c6a3c37a5bbb5717f73e88a4e3f4a40bd6213
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-07-09 23:53:58 -0400

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+  stringArray = string.split("");
+  reversedArray = stringArray.reverse();
+  reversedString = reversedArray.join("");
+  return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
