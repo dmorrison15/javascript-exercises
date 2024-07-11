201d1d85fc9a5e377456b27d50eef5f65568576a
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-07-10 21:26:48 -0400

    Update fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = function() {
-
+  const fibSequence = [1, 1];
+  for(let i = 2; i < a; i++) {
+    fibSequence.push(fibSequence[i - 1] + fibSequence[i - 2]);
+  }
+  return fibSequence[a - 1];
 };
 
 // Do not edit below this line
