9708809b51f3afa0be094846b24965528c45fde9
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-07-10 21:11:43 -0400

    Update palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let simpleString = string.toLowerCase().replace(" ", "");
+  let reversedString = simpleString.split("").reverse().join("");
+  return simpleString === reversedString;
 };
 
 // Do not edit below this line
