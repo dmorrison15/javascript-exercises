6078abde3744517b488d42fbc7b7e04ae2b39b88
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-07-10 20:51:04 -0400

    Update all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...args) {
+	let sum = 0;
+	for(let i = 0; i < args.length; i++) {
+		sum += args[i];
+	}
+	return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if(array.isEmpty()) {
+		return 0;
+	}
+	let sum = 0;
+	for(let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+	let product = 1;
+	for(let i = 0; i < args.length; i++) {
+		product *= args[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+	for(let i = a; i > 0; i--) {
+		product *= i;
+	}
+	return product;
 };
 
 // Do not edit below this line
